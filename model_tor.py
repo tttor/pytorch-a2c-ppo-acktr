b3a254305adc0a64c8cbcde13d990b0235234af8
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-07-16 23:33:17 +1000

    init critic output in hidden net

--- model_tor.py
+++ model_tor.py
@@ -9,11 +9,11 @@ class ActorCriticNetwork(nn.Module):
         super(ActorCriticNetwork, self).__init__()
 
         hidden_dim = 64
-        self.hidden_net = ActorCriticHiddenNetwork(input_dim, hidden_dim)
+        self.hidden_net = ActorCriticHiddenNetwork(input_dim, hidden_dim, critic_output_dim)
 
         # Output networks
         self.actor_output_net = GaussianDistributionNetwork(hidden_dim, actor_output_dim)
-        self.critic_output_net = init_param_openaibaselines(nn.Linear(hidden_dim, critic_output_dim))
+        # self.critic_output_net = init_param_openaibaselines(nn.Linear(hidden_dim, critic_output_dim)) # TODO: uncomment me, rm the one in hiddenNet
         self.state_size = critic_output_dim # TODO: make naming more descriptive
 
     def forward(self, inputs, states, masks):
@@ -32,15 +32,10 @@ class ActorCriticNetwork(nn.Module):
     def _forward(self, observ):
         hidden_actor = self.hidden_net.actor_hidden_net(observ)
         hidden_critic = self.hidden_net.critic_hidden_net(observ)
-        print(hidden_critic)
-        for n, p in self.hidden_net.critic_hidden_net.named_parameters():
-            print(n)
-            print(p.data)
-
-        return self.critic_output_net(hidden_critic), hidden_actor
+        return self.hidden_net.critic_output_net(hidden_critic), hidden_actor
 
 class ActorCriticHiddenNetwork(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
+    def __init__(self, input_dim, hidden_dim, critic_output_dim):
         super(ActorCriticHiddenNetwork, self).__init__()
 
         self.actor_hidden_net = nn.Sequential(
@@ -57,7 +52,7 @@ class ActorCriticHiddenNetwork(nn.Module):
             nn.Tanh()
         )
 
-    def forward(self, inputs, states, masks):
+        self.critic_output_net = init_param_openaibaselines(nn.Linear(hidden_dim, critic_output_dim)) # TODO: move to ActorCriticNet()
+
+    def forward(self, observ):
         raise NotImplementedError
-
-
